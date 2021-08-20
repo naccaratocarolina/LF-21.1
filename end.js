575ebc085d1f03e84ecf26e0b8e1ceeb8b5d0889
naccaratocarolina <naccaratocarolina@gmail.com>
2021-08-20 10:19:56 -0300

    + Logica do jogo + CSS finalizados

--- end.js
+++ end.js
@@ -1,14 +1,14 @@
 // Definindo constantes
-const MAX_HIGH_SCORES = 5;
+const MAX_HIGH_SCORES = 5; // numero maximo de usuarios no rank
 
 // Acessando objetos do HTML
-const username = document.querySelector('#username')
-const saveScoreBtn = document.querySelector('#saveScoreBtn')
-const finalScore = document.querySelector('#finalScore')
-const lastScore = localStorage.getItem('lastScore')
+const username = document.querySelector('#username');
+const saveScoreBtn = document.querySelector('#saveScoreBtn');
+const finalScore = document.querySelector('#finalScore');
+const lastScore = localStorage.getItem('lastScore');
 
 // Acessando o local storage (que tem o lastScore salvo)
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 finalScore.innerText = lastScore;
 
@@ -17,19 +17,21 @@ username.addEventListener('keyup', () => {
 });
 
 saveHighScore = e => {
-	e.preventDefault();
+    e.preventDefault();
 
-	const score = {
-		score: mostRecentScore,
-		name: username.value
-	};
+    const score = {
+        score: lastScore,
+        name: username.value
+    };
 
-	highScores.push(score);
+    highScores.push(score);
 
-	highScores.sort((a, b) => {
-		return b.score - a.score
-	});
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    });
 
-	localStorage.setItem('highScores', JSON.stringify(highScores));
-	window.location.assign('./');
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('./highscores.html');
 };
--- game.js
+++ game.js
@@ -19,40 +19,62 @@ let availableQuestions = [];
 // Questoes do quizz
 let questions = [
 	{
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question:"The tallest building in the world is located in which city?",
-        choice1: "Dubai",
-        choice2: "New York",
-        choice3: "Shanghai",
-        choice4: "None of the above",
-        answer: 1,
-    },
-    {
-        question: "What percent of American adults believe that chocolate milk comes from brown cows?",
-        choice1: "20%",
-        choice2: "18%",
-        choice3: "7%",
-        choice4: "33%",
-        answer: 3,
-    },
-    {
-        question: "Approximately what percent of U.S. power outages are caused by squirrels?",
-        choice1: "10-20%",
-        choice2: "5-10%",
-        choice3: "15-20%",
-        choice4: "30%-40%",
-        answer: 1,
-    }
+  	 question: 'What is 2 + 2?',
+     choice1: '2',
+     choice2: '4',
+     choice3: '21',
+     choice4: '17',
+     answer: 2,
+		 explanation: 'aasdsad' 
+  },
+  {
+  	 question:"The tallest building in the world is located in which city?",
+     choice1: "Dubai",
+     choice2: "New York",
+     choice3: "Shanghai",
+     choice4: "None of the above",
+     answer: 1,
+		 explanation: '12213123'
+  },
+  {
+     question: "What percent of American adults believe that chocolate milk comes from brown cows?",
+     choice1: "20%",
+     choice2: "18%",
+     choice3: "7%",
+     choice4: "33%",
+     answer: 3,
+		 explanation: 'pqpqpqpq'
+  },
+  {
+     question: "Approximately what percent of U.S. power outages are caused by squirrels?",
+     choice1: "10-20%",
+     choice2: "5-10%",
+     choice3: "15-20%",
+     choice4: "30%-40%",
+     answer: 1,
+		 explanation: '123was'
+  },
+  {
+     question: "Pergunta extra",
+     choice1: "a",
+     choice2: "b",
+     choice3: "c",
+     choice4: "d",
+     answer: 1,
+		 explanation: 'aaaaaaaaaaa'
+  },
+  {
+     question: "Pergunta extra 2",
+     choice1: "a",
+     choice2: "b",
+     choice3: "c",
+     choice4: "d",
+     answer: 4,
+		 explanation: 'bbbbbbbbb'
+  },
 ];
 
-// Logica do jogo
+/* Funcao que inicializa o jogo  */
 start = () => {
 	questionCounter = 0;
 	score = 0;
@@ -60,23 +82,31 @@ start = () => {
 	getNextQuestion();
 };
 
+/* 
+ * Implementa a logica de avancar para a proxima pergunta, assim como fazer o display
+ * do texto das perguntas e suas opcoes
+ */
 getNextQuestion = () => {
 	// Condicoes para encerrar o jogo
-	if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+	if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
 		// Salva a pontuacao no local storage
 		localStorage.setItem('lastScore', score);
 
 		return window.location.assign('./end.html');
 	}
 
+	// Inicializa o jogo, incrementando o contador e fazendo o display
+	// da barra de progresso de acordo com a questao atual
 	questionCounter++;
 	progressText.innerText = `Questão ${questionCounter} of ${MAX_QUESTIONS}`;
 	progressBar.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 	
+	// Faz o display da pergunta atual
 	const questionIndex = Math.floor(Math.random() * availableQuestions.length);
 	currentQuestion = availableQuestions[questionIndex];
 	question.innerText = currentQuestion.question;
 
+	// Faz o display das opcoes da pergunta atual
 	choices.forEach(choice => {
 		const number = choice.dataset['number'];
 		choice.innerText = currentQuestion['choice' + number];
@@ -84,34 +114,56 @@ getNextQuestion = () => {
 
 	availableQuestions.splice(questionIndex, 1);
 
+	// Seta a variavel de controle como true
 	isAcceptiongAnswers = true;
 };
 
+/*
+ * Realiza a logica de definir qual opcao eh a correta, de acordo com o array de perguntas
+ * (aplicando o CSS correspondente), assim como define a animacao de avancar para uma proxima 
+ * pergunta. No caso do usuario nao acertar a resposta correta, faz o display de um popup com
+ * a explicacao.
+ */
 choices.forEach(choice => {
 	choice.addEventListener('click', choice => {
+		// Se a variavel de controle nao for true, interrompe a funcao
 		if (!isAcceptiongAnswers) return;
-
+		
+		// Salva as informacoes da opcao clicada pelo usuario
 		isAcceptiongAnswers = false;
 		const selectedChoice = choice.target;
 		const selectedAnswer = selectedChoice.dataset['number'];
 
+		// Variavel booleana de controle para aplicar o estilo correto de acordo com a
+		// resposta correta de cada questao
 		let styleToAply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
+		// No caso do usuario escolher a resposta correta, incrementa a sua pontuacao
 		if (styleToAply == 'correct') 
 			incrementScore(SCORE_PER_QUESTION);
 
 		selectedChoice.parentElement.classList.add(styleToAply);
-
+		
+		// Implementa a animacao
 		setTimeout(() => {
 			selectedChoice.parentElement.classList.remove(styleToAply);
+
+			// Dispara um alerta com a explicacao caso a resposta escolhida for incorreta
+			if (styleToAply == 'incorrect') alert(currentQuestion['explanation']);
+
 			getNextQuestion();
 		}, 500)
 	})
 });
 
+/* 
+ * Acessa a variavel do jogo score e a incrementa, dado o valor (number)
+ * a ser incrementado (SCORE_PER_QUESTION)
+*/
 incrementScore = number => {
 	score += number;
 	scoreText.innerText = score;
 };
 
+// Inicializa o jogo
 start();
